475bb2506f4d71f67a702c0e23e60629192b0ad9
mjdierkes <mjdierkes@msn.com>
2022-05-28 11:33:36 -0500

    Initial Commit

---
+++ Habitable/HabitableApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitableApp.swift
+//  Habitable
+//
+//  Created by Mason Dierkes on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
